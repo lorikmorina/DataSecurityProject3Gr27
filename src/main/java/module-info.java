791e3855d54140c95b868aa0b43f6a9faa364d7d
Lorik Morina <lorik.morina1@student.uni-pr.edu>
2023-05-10 13:23:23 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.datasecurityproject3gr27 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.datasecurityproject3gr27 to javafx.fxml;
+    exports com.example.datasecurityproject3gr27;
+}
