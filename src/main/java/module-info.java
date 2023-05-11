12274014bc7d1365d29843458b86e16ddfcc80a0
Lorik Morina <lorik.morina1@student.uni-pr.edu>
2023-05-11 14:23:40 +0200

    init

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.datasecurityproject3gr27 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.commons.codec;
 
 
     opens com.example.datasecurityproject3gr27 to javafx.fxml;
